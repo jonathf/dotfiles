d685746e56a861c2d07981caec4ea19a86489dd9
Jonathan Feinberg <jonathf@gmail.com>
2020-04-26 10:55:55 +0200

    refactor and more configs

--- README.rst
+++ README.rst
@@ -6,15 +6,72 @@ Personal configurations for the following programs:
 * Git
 * IPython
 * Matplotlib
-* URxvt
+* URxvt/Alacritty
 
 Installation
 ------------
 
-Installation is done by deleting the old configurations, then create new
-symbolic link to various files and folders. This can be achieved by running the
-configuration script:
+Note that installation is done by deleting the old configurations, then create
+new symbolic link to various files and folders.
+If there are something important you want to keep there, save the
+relevant configurations first.
 
-.. code:: bash
+To install, follow the steps:
 
-    sh configure.sh
+1. Clone the repository to your system. It doesn't matter where, though it is
+   expected that the folder is not removed, so perhaps:
+
+   .. code:: bash
+
+       git clone https://github.com/jonathf/dotfiles ~/.config/dotfiles
+
+2. Run the configuration script:
+
+    .. code:: bash
+
+       sh ~/.config/configure.sh
+
+3. (Optional) Install ``direnv`` by running the code on Arch Linux:
+
+   .. code:: bash
+
+      fish -c "aurbuild direnv/direnv-archlinux"
+
+   On systems without root access, run the following command instead:
+
+   .. code:: bash
+
+      bash ~/.config/fish/direnv_install.sh
+
+4. (Optional) Install ``pyenv`` by cloning the repository:
+
+   .. code:: bash
+
+      git clone https://github.com/pyenv/pyenv ~/.pyenv
+
+Features
+--------
+
+``git diff``
+    An updated fancy ``git diff`` with nice colors and stuff.
+``git edit``
+    Use your default ``$EDITOR`` to edit all "dirty" files in your
+    repository. This can be used both to "resume" editing where you left off,
+    and to go straight to conflict files during merge/rebase.
+``git gblame``/``git gcheckout``/``git gmerge``
+    Same as ``git {blame,checkout,merge}``, but uses Github pull-request as reference.
+``git git git``
+    Multiple repeating git is interpreted as one.
+``git ll``
+    Git log with single line commits, graph tree and pretty colors.
+``git prune``
+    Remove all branches flagged as merged that (usually) only exist as
+    artifacts after completed pull-requests.
+``<alt-z>``
+    Detach from the prompt using the ``urxvt`` extension
+    ``vim-scrollback``. great for scrolling backwards in a (bastardized)
+    vi-mode, and select content from what is visually present on the
+    screen. Great for getting server content over to the clipboard.
+``<ctrl--><ctrl-+><ctrl-0>``
+    Zooming out, in and back to default using the ``urxvt`` extension
+    ``reactivity`` and natively in ``alactritty``.
--- configure.sh
+++ configure.sh
@@ -3,33 +3,79 @@
 CFG_ROOT=$(dirname $(readlink -f $0))
 
 # Git
+if [[ ! -h ~/.git_template ]]; then
+  [[ -d ~/.git_template ]] && rm -rfv ~/.git_template
+  ln -sv $CFG_ROOT/git/git_template ~/.git_template
+fi
 
-[[ -d ~/.git_template ]] && rm -rfv ~/.git_template
-ln -sv $CFG_ROOT/git_template ~/.git_template
+if [[ ! -h ~/.gitconfig ]]; then
+  [[ -f ~/.gitconfig ]] && rm -v ~/.gitconfig
+  ln -svf $CFG_ROOT/git/gitconfig ~/.gitconfig
+fi
 
-[[ -f ~/.gitconfig ]] && rm -v ~/.gitconfig
-ln -svf $CFG_ROOT/gitconfig ~/.gitconfig
-
-[[ -f ~/.gitignore ]] && rm -v ~/.gitignore
-ln -svf $CFG_ROOT/gitignore ~/.gitignore
+if [[ ! -h ~/.gitignore ]]; then
+  [[ -f ~/.gitignore ]] && rm -v ~/.gitignore
+  ln -svf $CFG_ROOT/git/gitignore ~/.gitignore
+fi
 
 [[ ! -d ~/.local/bin ]] && mkdir -vp ~/.local/bin
-ln -svf $CFG_ROOT/git-blame-pr ~/.local/bin
-ln -svf $CFG_ROOT/git-fancy-diff ~/.local/bin
+[[ ! -f ~/.local/bin/git-blame-pr ]] && ln -svf $CFG_ROOT/git/git-blame-pr ~/.local/bin
+[[ ! -f ~/.local/bin/git-fancy-diff ]] && ln -svf $CFG_ROOT/git/git-fancy-diff ~/.local/bin
 
-# Python
-[[ -d ~/.ipython ]] && rm -rfv ~/.ipython
-ln -sv $CFG_ROOT/ipython ~/.ipython
+if [[ ! -h ~/.ipython ]]; then
+  [[ -d ~/.ipython ]] && rm -rfv ~/.ipython
+  ln -sv $CFG_ROOT/ipython ~/.ipython
+fi
 
-[[ ! -d ~/.config/matplotlib ]] && mkdir -vp ~/.config/matplotlib
-ln -svf $CFG_ROOT/matplotlibrc ~/.config/matplotlib/matplotlibrc
+if [[ ! -h ~/.config/matplotlib ]]; then
+  [[ -d ~/.config/matplotlib ]] && rm -rfv ~/.config/matplotlib
+  ln -sv $CFG_ROOT/matplotlib ~/.config/matplotlib
+fi
 
-# Terminal emulator
-[[ -f ~/.Xresources ]] && rm -v ~/.Xresources
-ln -svf $CFG_ROOT/Xresources ~/.Xresources
+if [[ ! -h ~/.Xresources ]]; then
+  [[ -f ~/.Xresources ]] && rm -v ~/.Xresources
+  ln -svf $CFG_ROOT/Xresources ~/.Xresources
+fi
 
-[[ -d ~/.urxvt ]] && rm -rfv ~/.urxvt
-ln -sv $CFG_ROOT/urxvt ~/.urxvt
+if [[ ! -h ~/.urxvt ]]; then
+  [[ -d ~/.urxvt ]] && rm -rfv ~/.urxvt
+  ln -sv $CFG_ROOT/urxvt ~/.urxvt
+fi
 
-[[ -f ~/.config/user-dirs.dirs ]] && rm -v ~/.config/user-dirs.dirs
-ln -svf $CFG_ROOT/user-dirs.dirs ~/.config/user-dirs.dirs
+if [[ ! -h ~/.config/alacritty ]]; then
+  [[ -d ~/.config/alacritty ]] && rm -rfv ~/.config/alacritty
+  ln -sv $CFG_ROOT/alacritty ~/.config/alacritty
+fi
+
+if [[ ! -h ~/.config/user-dirs.dirs ]]; then
+  [[ -f ~/.config/user-dirs.dirs ]] && rm -v ~/.config/user-dirs.dirs
+  ln -svf $CFG_ROOT/user-dirs.dirs ~/.config/user-dirs.dirs
+fi
+
+if [[ ! -h ~/.config/tridactyl ]]; then
+  [[ -d ~/.config/tridactyl ]] && rm -rfv ~/.config/tridactyl
+  ln -sv $CFG_ROOT/tridactyl ~/.config/tridactyl
+fi
+
+if [[ ! -h ~/.config/rofi ]]; then
+  [[ -d ~/.config/rofi ]] && rm -rfv ~/.config/rofi
+  ln -sv $CFG_ROOT/rofi ~/.config/rofi
+fi
+
+if [[ ! -h ~/.config/zathura ]]; then
+  [[ -d ~/.config/zathura ]] && rm -rfv ~/.config/zathura
+  ln -sv $CFG_ROOT/zathura ~/.config/zathura
+fi
+
+if [[ ! -h ~/.bashrc ]]; then
+  [[ -f ~/.bashrc ]] && rm -v ~/.bashrc
+  ln -svf $CFG_ROOT/bash/bashrc ~/.bashrc
+fi
+if [[ ! -h ~/.bash_aliases ]]; then
+  [[ -f ~/.bash_aliases ]] && rm -v ~/.bash_aliases
+  ln -svf $CFG_ROOT/bash/bash_aliases ~/.bash_aliases
+fi
+if [[ ! -h ~/.bash_prompt ]]; then
+  [[ -f ~/.bash_prompt ]] && rm -v ~/.bash_prompt
+  ln -svf $CFG_ROOT/bash/bash_prompt ~/.bash_prompt
+fi
