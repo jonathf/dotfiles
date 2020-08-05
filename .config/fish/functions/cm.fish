2e7a9a4bf95c7b5592e53560a1d91e2e0871b4e7
Jonathan Feinberg <jonathf@gmail.com>
2020-08-05 11:18:42 +0200

    fix to fish

--- .config/fish/functions/cm.fish
+++ .config/fish/functions/cm.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function cm --description 'alias cm git commit' --wraps 'git commit'
+function cm --description 'alias cm git commit' --wraps 'git commit -f'
     $HOME/.local/bin/cm $argv;
 end
