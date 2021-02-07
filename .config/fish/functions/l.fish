349f53ef978735a46b5eb2f4e889ea80d80fd198
Jonathan Feinberg <jonathf@gmail.com>
2021-02-07 11:09:00 +0100

    update git ll->graph

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function l --description 'alias l git ll -20' --wraps 'git ll -20'
-    git --no-pager ll -20 $argv;
+function l --description 'alias l git graph -20' --wraps 'git graph -20'
+    git --no-pager graph -20 $argv;
 end
