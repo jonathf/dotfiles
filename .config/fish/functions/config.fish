7cedd4d70a2cab1e1b54df87ea106acf7abd8597
Jonathan Feinberg <jonathf@gmail.com>
2021-04-13 14:51:22 +0200

    renaming: _git -> config

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function _git --description 'alias git git' --wraps 'git --git-dir=~/.config/dotfiles --work-tree=~'
-    ~/.local/bin/_git $argv
+    ~/.local/bin/config $argv
 end
