5c1399fabe1a6433a65631943d1e709774cadd7b
Jonathan Feinberg <jonathf@gmail.com>
2021-11-29 23:29:44 +0100

    bugfixes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -7,4 +7,5 @@ require("layout")
 root.keys(require("mapping").rootkeys)
 
 awful.spawn.once("xcompmgr -cF")
+awful.spawn.once("setxkbmap no")
 beautiful.init(awful.util.getdir("config").."theme.lua")
