76e7ef9c1a25c9e5f07a112f146a814e5623f6c6
Jonathan Feinberg <jonathf@gmail.com>
2022-08-10 12:07:34 +0200

    Fish: add missing .envrc when installing py

--- .config/fish/functions/install_python_environment.fish
+++ .config/fish/functions/install_python_environment.fish
@@ -1,7 +1,10 @@
 function install_python_environment --description 'create python virtual environment and install some base dependencies'
   python$argv[1].$argv[2] -m venv .$argv[1]$argv[2]
   source .$argv[1]$argv[2]/bin/activate.fish
-  mv ~/.config/pip/pip.conf{,.lock}
+  mv ~/.config/pip/pip.conf{,_}
   pip install -Ur ~/.config/requirements.txt
-  mv ~/.config/pip/pip.conf{.lock,}
+  mv ~/.config/pip/pip.conf{_,}
+  if test ! -f .envrc
+    echo "source .$argv[1]$argv[2]/bin/activate" > .envrc
+  end
 end
