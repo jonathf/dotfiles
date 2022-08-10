23160063809ffa65121bb1d85d7e26d58013058f
Jonathan Feinberg <jonathf@gmail.com>
2022-08-10 12:19:13 +0200

    Ipython: update auto-magic command

--- .ipython/profile_default/startup/10-importer.py
+++ .ipython/profile_default/startup/10-importer.py
@@ -3,10 +3,14 @@ from IPython import get_ipython
 ipython = get_ipython()
 
 try:
-    ipython.magic("load_ext pyflyby")
+    ipython.run_line_magic("load_ext", "pyflyby")
 except ModuleNotFoundError:
     print("pyflyby is missing!")
 try:
-    ipython.magic("load_ext rich")
+    ipython.run_line_magic("load_ext", "rich")
 except ModuleNotFoundError:
     print("rich is missing!")
+try:
+    ipython.run_line_magic("autoreload", "2")
+except ModuleNotFoundError:
+    print("autoreload is missing!")
