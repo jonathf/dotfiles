17883dac70ecb7cb57a65ced9e7d0e3d2bb8e2c6
Jonathan Feinberg <jonathf@gmail.com>
2022-08-10 12:21:00 +0200

    Pyflyby: More auto-imports

--- .config/pyflyby.py
+++ .config/pyflyby.py
@@ -1,23 +1,10 @@
-import os
-import sys
-
-import typing
 from typing import Any, Callable, Dict, Generator, Iterator, List, Optional, Sequence, Set, Tuple, Union
+from pathlib import Path
 
-import numpy
-import numpy as np
-from numpy import array, ndarray
-
-import pandas
-import pandas as pd
-from pandas import DataFrame, Series
-
-import matplotlib.pyplot
+import h5py as h5
 from matplotlib import pyplot
 from matplotlib import pyplot as plt
-
-import scipy
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
 import scipy as sp
-
-import h5py
-import h5py as h5
